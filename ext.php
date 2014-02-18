13aa11b5cca3ce566dfa49abb4959e84c7261e13
ForumHulp <info@forumhulp.com>
2014-02-18 20:47:11 +0100

    Create ext.php

---
+++ ext.php
@@ -0,0 +1,14 @@
+<?php
+/**
+*
+* @package Inactive Users
+* @copyright (c) 2014 ForumHulp.com
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbb\inactive_users;
+
+class ext extends \phpbb\extension\base
+{
+}
