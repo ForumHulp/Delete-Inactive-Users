f9a3226796b0d86d2d9386501a983254096a05ff
Forumhulp.com <info@forumhulp.com>
2014-02-22 22:44:39 +0100

    Update inactive_users.php

--- cron/task/core/inactive_users.php
+++ cron/task/core/inactive_users.php
@@ -52,7 +52,7 @@ class inactive_users extends \phpbb\cron\task\base
 		$expire_date = time() - ($this->config['inactive_users_days'] * 86400);
 		$user_list = array();
 	
-		$sql = 'SELECT user_id, username, user_regdate FROM ' . USERS_TABLE . ' WHERE user_type = USER_INACTIVE AND user_new = 1 AND user_regdate < ' . $expire_date;
+		$sql = 'SELECT user_id, username, user_regdate FROM ' . USERS_TABLE . ' WHERE user_type = ' . USER_INACTIVE . ' AND user_new = 1 AND user_regdate < ' . $expire_date;
 		$result = $db->sql_query($sql);
 	
 		while ($row = $db->sql_fetchrow($result))
@@ -71,7 +71,7 @@ class inactive_users extends \phpbb\cron\task\base
 		{
 			add_log('admin', 'NO_INACTIVE_USERS');
 		}
-		set_config('inactive_users_last_gc', time(), true);
+		$this->config->set('inactive_users_last_gc', time());
 	}
 
 	/**
