8fe76d91158def092fc87172877ead99fad4b851
John <info@forumhulp.com>
2014-09-02 20:50:34 +0200

    Language

---
+++ language/en/info_acp_deleteinactiveusers.php
@@ -0,0 +1,23 @@
+<?php
+/**
+*
+* @package Inactive Users
+* @copyright (c) 2014 ForumHulp.com
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'INACTIVE_USERS_DAYS' => 'Delete inactive users',
+	'INACTIVE_USERS_DAYS_EXPLAIN'	=> 'Days registered before cron wil delete new inacive users who never where logged in'
+));
