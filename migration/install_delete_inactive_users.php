f175afdff18d3a3bc3772abf6415a13f85ac5613
John <info@forumhulp.com>
2014-09-17 21:23:23 +0200

    Migration folder change RC4

--- migration/install_delete_inactive_users.php
+++ migration/install_delete_inactive_users.php
@@ -7,13 +7,13 @@
 *
 */
 
-namespace forumhulp\DeleteInactiveUsers\migrations;
+namespace forumhulp\DeleteInactiveUsers\migration;
 
 class install_delete_inactive_users extends \phpbb\db\migration\migration
 {
 	public function effectively_installed()
 	{
-		return isset($this->config['delete_inactive_users_version']) && version_compare($this->config['delete_inactive_users_version'], '3.1.0', '>=');
+		return isset($this->config['delete_inactive_users_version']) && version_compare($this->config['delete_inactive_users_version'], '3.1.0.RC4', '>=');
 	}
 
 	static public function depends_on()
@@ -27,7 +27,7 @@ class install_delete_inactive_users extends \phpbb\db\migration\migration
 			array('config.add', array('delete_inactive_users_gc', 86400)),
 			array('config.add', array('delete_inactive_users_last_gc', '0', 1)),
 			array('config.add', array('delete_inactive_users_days', 30)),
-			array('config.add', array('delete_inactive_users_version', '3.1.0'))
+			array('config.add', array('delete_inactive_users_version', '3.1.0.RC4'))
 		);
 	}
 }
