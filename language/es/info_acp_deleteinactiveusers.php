9dee1c446e2875ab43fd0fa4d1223066329f6bb2
phpBB España <webmaster@phpbb-es.com>
2014-11-04 21:46:34 +0100

    Translation spanish
    Translations spanish

---
+++ language/es/info_acp_deleteinactiveusers.php
@@ -0,0 +1,23 @@
+<?php
+/**
+*
+* @package Inactive Users
+* @copyright (c) 2014 ForumHulp.com
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'INACTIVE_USERS_DAYS' => 'Borrar usuarios inactivos',
+	'INACTIVE_USERS_DAYS_EXPLAIN'	=> 'Días registrado antes de que el cron elimine nuevos usuarios inactivos, que nunca iniciarón sesión'
+));
