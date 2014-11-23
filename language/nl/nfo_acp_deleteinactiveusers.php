bf750eee4698a881207503e3c934b9bb94c273c8
WiMaPe <WiMaPe@users.noreply.github.com>
2014-11-23 11:47:01 +0100

    Update nfo_acp_deleteinactiveusers.php
    Anglicisme blindness; forgot to translate inactive to inactieve and users to gebruikers in one sentence

--- language/nl/nfo_acp_deleteinactiveusers.php
+++ language/nl/nfo_acp_deleteinactiveusers.php
@@ -15,6 +15,6 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 $lang = array_merge($lang, array(
-	'INACTIVE_USERS_DAYS' => 'Verwijder inactive users',
+	'INACTIVE_USERS_DAYS' => 'Verwijder inactieve gebruikers',
 	'INACTIVE_USERS_DAYS_EXPLAIN'	=> 'Dagen geregistreerd alvorens een herinnering te sturen. Een week later zullen de nieuwe inactive gebruikers welke nooit ingelogd zijn geweest, verwijderd worden.'
 ));
