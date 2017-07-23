c0ae72c1cebc8c2c6756e0bab7f0ad316d783b77
ThE KuKa <thekuka@gmail.com>
2017-07-23 15:17:30 +0200

    Spanish update.
    Spanish update.

--- language/es/info_acp_deleteinactiveusers.php
+++ language/es/info_acp_deleteinactiveusers.php
@@ -18,8 +18,8 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'INACTIVE_USERS_DAYS'			=> 'Borrar usuarios inactivos',
+	'INACTIVE_USERS_DAYS'		=> 'Borrar usuarios inactivos',
 	'INACTIVE_USERS_DAYS_EXPLAIN'	=> 'Días registrado antes de que el cron elimine nuevos usuarios inactivos, que nunca iniciarón sesión',
-	'LOG_INACTIVE_USERS'			=> '<strong>Delete inactive users</strong><br />» %s',
-	'INACTIVE_USERS_NOTICE'			=> '<div class="phpinfo"><p class="entry">Config settings in %1$s » %2$s » %3$s.</p></div>'
+	'LOG_INACTIVE_USERS'		=> '<strong>Usuarios inactivos borrados</strong><br />» %s',
+	'INACTIVE_USERS_NOTICE'		=> '<div class="phpinfo"><p class="entry">Ajustes de configuración en %1$s » %2$s » %3$s.</p></div>'
 ));
